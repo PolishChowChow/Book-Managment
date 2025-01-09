7b57af0a8c34bb30163ac6fd8fb1c95e92642931
PolishChowChow <polishchowchow@MacBook-Air-PolishChowChow.local>
2025-01-09 22:39:48 +0100

    feat: initialized relation between customers and  reservations tables

---
+++ server/src/customers/customer.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Customer {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  emailAddress: string;
+
+  @Column()
+
+  phoneNumber: string;
+
+  @Column({ type: 'date' })
+  dateOfBirth: string;
+}
